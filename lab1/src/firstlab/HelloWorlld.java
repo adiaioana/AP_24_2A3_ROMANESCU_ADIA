51a3c14a6649f760da6ded26bd7b20f244376a25
Adia <adiaioana@gmail.com>
2024-03-01 13:36:16 +0200

    Added homework

--- lab1/src/firstlab/HelloWorlld.java
+++ lab1/src/firstlab/HelloWorlld.java
@@ -1,10 +1,18 @@
 package firstlab;
-
+import java.util.*;
 public class HelloWorlld {
 	
 	public static void main(String args[]) {
+		long startTime = System.nanoTime();
 		HelloWorlld HL=new HelloWorlld();
-		HL.compulsory();
+		//HL.compulsory();
+		//for(int k=1; k<=100; ++k) {
+			int k=17;
+			String hihi[]= {"10", "1000", ""};
+			hihi[2]=Integer.toString(k);
+			HL.homework(hihi, startTime);
+			
+		//}
 	}
 	public void compulsory() {
 		String[] languages={"C", "C++", "C#", "Python", "Go", "Rust", "JavaScript", "PHP", "Swift", "Java"};
@@ -30,5 +38,53 @@ public class HelloWorlld {
 			k=sumcif;
 		}
 		return k;
+	}
+	public void homework(String args[], long ST) {
+		Vector<Integer> int_args=new Vector<Integer>();
+		Vector<Integer> K_List=new Vector<Integer>();
+		for(String it: args) {
+			int number=Integer.valueOf(it);
+			int_args.add(number);
+		}
+		
+		int k=int_args.get(2);
+		int a=int_args.get(0);
+		int b=int_args.get(1);
+		
+		for(int nr=a; nr<=b; ++nr) 
+			if(k_reductibil(nr,k)==k){
+				K_List.add(nr);
+		}
+		if(K_List.isEmpty())
+			return ;
+		System.out.println("Starting printing "+k+"-reductible numbers>");
+		for(Integer it: K_List) {
+			System.out.print(it+", ");
+		}
+		System.out.println();
+		long ET=System.nanoTime(); long dur=ET-ST;
+		System.out.println("The running time (in nano seconds) is= "+ dur);
+	}
+	public short k_reductibil(int value, int maybe) {
+		
+		int sum_cif=0;
+		int nr_it=0;
+		while(value>9) {
+			int aux=value;
+			nr_it++;
+			while(aux>0) {
+				sum_cif+=(aux%10)*(aux%10);
+				aux/=10;
+			}
+			aux=sum_cif;
+			if(maybe==aux)
+				return (short)maybe;
+			
+			value=aux;
+			if(nr_it>100)
+				break;
+		}
+		
+		return (short)value;
 	}
 }
